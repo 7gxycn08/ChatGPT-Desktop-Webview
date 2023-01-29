c4bf03ae87533e345a3d7d6983b7db1970a08807
7gxycn08 <121936658+7gxycn08@users.noreply.github.com>
2023-01-29 09:38:27 +0400

    Updated to v1.0.0.4
    PyQt6 now retains login upon GUI restart.

--- ChatGPT.py
+++ ChatGPT.py
@@ -14,26 +14,20 @@ try:
 except:
        pass
 
-#Enable this class to set a different User Agent (Might cause issues with Cloudflare)
-# class UserAgentInterceptor(QWebEngineUrlRequestInterceptor):
-#     def interceptRequest(self, info: QWebEngineUrlRequestInfo):
-#         user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/108.0 Mobile/15E148 Safari/605.1.15"
-#         info.setHttpHeader(QByteArray(b"user-agent"), QByteArray.fromRawData(user_agent.encode()))
-
 class ChatGPT_Main(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(1200,500,600,520)
         self.setWindowIcon(QIcon('Resources\\icon.ico'))
-        self.setWindowTitle('ChatGPT Desktop Webview v1.0.0.3')
-        self.webview = QWebEngineView()
-        self.profile = QWebEngineProfile.defaultProfile()
+        self.setWindowTitle('ChatGPT Desktop Webview v1.0.0.4')
+        self.profile = QWebEngineProfile('ChatGPTProfile')
         self.settings = self.profile.settings()
-        self.settings.setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard,True) #Fixes the copy button not working issue.
+        self.settings.setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard,True)
+        self.webview = QWebEngineView(self.profile)
         self.setCentralWidget(self.webview)
         self.webview.setGeometry(1200,500,600,520)
-        self.webview.setZoomFactor(1)
-        self.url = QUrl("https://chat.openai.com")
+        self.webview.setZoomFactor(0.7)
+        self.url = QUrl("https://chat.openai.com/chat")
         self.webview.load(self.url)
         self.show()
         self.webview.show()
