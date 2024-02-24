a20635ae12c98a07e6bdc8b1dfc137a887c07715
Andrew <121936658+7gxycn08@users.noreply.github.com>
2024-02-24 08:09:18 +0400

    Add files via upload

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,24 +1,29 @@
 import os
-from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import QUrl
-from PyQt6.QtWebEngineCore import (QWebEngineSettings, QWebEngineProfile, QWebEnginePage)
-from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QVBoxLayout
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import QUrl
+from PySide6.QtWebEngineCore import (QWebEngineSettings, QWebEngineProfile, QWebEnginePage)
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QApplication, QMainWindow
 from sys import argv, exit
 
 
 class CustomWebEnginePage(QWebEnginePage):
     def __init__(self, profile, parent=None):
         super().__init__(profile, parent)
+        self.popup_dialog = QMainWindow()
+        self.new_webview = QWebEngineView()
 
     def createWindow(self, _type):
-        self.new_webview = QWebEngineView()
         self.new_webview.setPage(QWebEnginePage(self.profile(), self.new_webview))
-        self.popup_dialog = QDialog()
-        self.popup_dialog.setWindowTitle("ChatGPT Desktop Webview v1.0.0.6")
+        self.new_webview.page().action(self.new_webview.page().WebAction.SavePage).setVisible(False)
+        self.new_webview.page().action(self.new_webview.page().WebAction.ViewSource).setVisible(False)
+        self.new_webview.page().action(self.new_webview.page().WebAction.Cut).setVisible(True)
+        self.new_webview.page().action(self.new_webview.page().WebAction.Copy).setVisible(True)
+        self.new_webview.page().action(self.new_webview.page().WebAction.Paste).setVisible(True)
+        self.new_webview.setZoomFactor(0.7)
+        self.popup_dialog.setWindowTitle("ChatGPT Desktop Webview v1.0.0.7")
         self.popup_dialog.setWindowIcon(QIcon('Resources/icon.ico'))
-        self.popup_dialog.setLayout(QVBoxLayout())
-        self.popup_dialog.layout().addWidget(self.new_webview)
+        self.popup_dialog.setCentralWidget(self.new_webview)
         self.popup_dialog.setGeometry(1200, 500, 600, 520)
         self.popup_dialog.show()
 
@@ -30,12 +35,15 @@ class ChatGptMain(QMainWindow):
         super().__init__()
         self.setGeometry(1200, 500, 600, 520)
         self.setWindowIcon(QIcon(r'Resources/icon.ico'))
-        self.setWindowTitle('ChatGPT Desktop Webview v1.0.0.6')
+        self.setWindowTitle('ChatGPT Desktop Webview v1.0.0.7')
         self.profile = QWebEngineProfile('ChatGPTProfile')
         self.profile.setPersistentStoragePath(fr"C:/Users/{os.getlogin()}/AppData/Local/ChatGPT")
         self.settings = self.profile.settings()
         self.settings.setAttribute(QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard, True)
         self.settings.setAttribute(QWebEngineSettings.WebAttribute.JavascriptEnabled, True)
+        self.settings.setAttribute(QWebEngineSettings.WebAttribute.Accelerated2dCanvasEnabled, True)
+        self.settings.setAttribute(QWebEngineSettings.WebAttribute.WebGLEnabled, True)
+        self.settings.setAttribute(QWebEngineSettings.WebAttribute.AllowWindowActivationFromJavaScript, True)
         self.webview = QWebEngineView(self)
         self.setCentralWidget(self.webview)
         self.webview.setPage(CustomWebEnginePage(self.profile, self.webview))
@@ -44,6 +52,7 @@ class ChatGptMain(QMainWindow):
         self.url = QUrl("https://chat.openai.com")
         self.webview.load(self.url)
         self.webview.page().action(self.webview.page().WebAction.SavePage).setVisible(False)
+        self.webview.page().action(self.webview.page().WebAction.ViewSource).setVisible(False)
         self.webview.page().action(self.webview.page().WebAction.Cut).setVisible(True)
         self.webview.page().action(self.webview.page().WebAction.Copy).setVisible(True)
         self.webview.page().action(self.webview.page().WebAction.Paste).setVisible(True)
